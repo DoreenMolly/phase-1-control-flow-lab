b4d72ba8255484eb39763d68c80c39c7da1fcd39
Doreen <wanjirudoreen13@gmail.com>
2022-06-06 21:49:08 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride){
+  let message
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  if (ride <=400 ) {
+    message = 'This one is on me!';
+  }
+  else if (ride >2500 ){
+    message = 'No can do.';
+  }
+    else if (ride >2000 ){
+      message = 'I will gladly take your thirty bucks.';
+    }
+      
+  return message
+    }
+  
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+}
+   
+  
+
+
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case tip = 'generous':
+    return 'Thank you so much.'
+    case tip = 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
